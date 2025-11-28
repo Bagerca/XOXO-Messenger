f9b656c6fa84ad0cc29d31b44489ce7170100e0e
Bagerca <bagrationbebia@gmail.com>
2025-11-28 14:53:40 +0300

    Update avatar.js

--- js/core/avatar.js
+++ js/core/avatar.js
@@ -1,6 +1,6 @@
 import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
 import gsap from 'https://unpkg.com/gsap@3.12.5/index.js';
-import { vertexShader, transitions } from './shaders.js';
+import { vertexShader, effects } from './shaders.js';
 
 export class AvatarRenderer {
     constructor(containerId, imageUrl, options = {}) {
@@ -12,12 +12,14 @@ export class AvatarRenderer {
         
         // Настройки по умолчанию
         this.options = Object.assign({
-            intensity: 0.3
+            effect: 'liquid', // liquid, glitch, pixel
+            intensity: 0.5
         }, options);
 
         this.init();
         this.loadImage(imageUrl);
         this.addEvents();
+        this.animate();
     }
 
     init() {
@@ -31,88 +33,71 @@ export class AvatarRenderer {
         this.renderer.setSize(this.width, this.height);
         this.container.appendChild(this.renderer.domElement);
 
-        // Генерация шума
+        // Текстура шума для Liquid
         const size = 128;
         const data = new Uint8Array(size * size * 4);
         for (let i = 0; i < size * size * 4; i++) data[i] = Math.random() * 255;
         this.dispTexture = new THREE.DataTexture(data, size, size, THREE.RGBAFormat);
         this.dispTexture.needsUpdate = true;
+        this.clock = new THREE.Clock();
     }
 
-    // Метод для динамической смены аватарки
+    // Смена картинки
     updateImage(url) {
         new THREE.TextureLoader().load(url, (tex) => {
             tex.minFilter = THREE.LinearFilter;
-            tex.generateMipmaps = false;
-            
-            // Если материал уже есть, просто обновляем текстуры
-            if (this.material) {
-                this.material.uniforms.texture1.value = tex;
-                this.material.uniforms.texture2.value = tex;
-                this.render();
-            } else {
-                this.createMesh(tex);
-            }
+            this.currentTexture = tex;
+            this.createMesh();
         });
     }
 
-    // Метод для обновления настроек (интенсивность)
+    // Смена настроек (шейдер, интенсивность)
     updateSettings(newOptions) {
         this.options = Object.assign(this.options, newOptions);
-        if (this.material) {
-            this.material.uniforms.intensity.value = this.options.intensity;
-            this.render();
-        }
+        if (this.currentTexture) this.createMesh();
     }
 
-    loadImage(url) {
-        this.updateImage(url);
-    }
+    loadImage(url) { this.updateImage(url); }
 
-    createMesh(texture) {
-        const transition = transitions.liquid;
+    createMesh() {
+        if (this.mesh) this.scene.remove(this.mesh);
+        
+        const effectData = effects[this.options.effect] || effects.liquid;
         
         this.uniforms = {
-            texture1: { value: texture },
-            texture2: { value: texture },
+            texture1: { value: this.currentTexture },
             disp: { value: this.dispTexture },
             dispFactor: { value: 0.0 },
-            intensity: { value: this.options.intensity }
+            intensity: { value: this.options.intensity },
+            time: { value: 0.0 }
         };
 
         this.material = new THREE.ShaderMaterial({
             uniforms: this.uniforms,
             vertexShader: vertexShader,
-            fragmentShader: transition.shader,
+            fragmentShader: effectData.shader,
             transparent: true
         });
 
         this.geometry = new THREE.PlaneGeometry(this.width, this.height, 1, 1);
         this.mesh = new THREE.Mesh(this.geometry, this.material);
         this.scene.add(this.mesh);
-        this.render();
     }
 
     addEvents() {
         this.container.addEventListener('mouseenter', () => {
-            gsap.to(this.uniforms.dispFactor, { 
-                value: 1, 
-                duration: 1, 
-                ease: "power2.out",
-                onUpdate: () => this.render() 
-            });
+            gsap.to(this.uniforms.dispFactor, { value: 1, duration: 0.8, ease: "power2.out" });
         });
         this.container.addEventListener('mouseleave', () => {
-            gsap.to(this.uniforms.dispFactor, { 
-                value: 0, 
-                duration: 1, 
-                ease: "power2.inOut",
-                onUpdate: () => this.render() 
-            });
+            gsap.to(this.uniforms.dispFactor, { value: 0, duration: 0.8, ease: "power2.inOut" });
         });
     }
 
-    render() {
+    animate() {
+        requestAnimationFrame(() => this.animate());
+        if (this.uniforms && this.uniforms.time) {
+            this.uniforms.time.value = this.clock.getElapsedTime();
+        }
         this.renderer.render(this.scene, this.camera);
     }
 }
