c8bc8bd7eddf73bfae1e4883e27be69c06b37910
Bagerca <bagrationbebia@gmail.com>
2025-11-28 14:05:08 +0300

    Update shaders.js

--- js/core/shaders.js
+++ js/core/shaders.js
@@ -1,1 +1,32 @@
+export const vertexShader = `
+    varying vec2 vUv;
+    void main() {
+        vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+`;
 
+export const transitions = {
+    liquid: {
+        uniforms: { intensity: 0.3 },
+        config: { duration: 0.9, ease: "power2.inOut" },
+        shader: `
+            varying vec2 vUv;
+            uniform sampler2D texture1;
+            uniform sampler2D texture2;
+            uniform sampler2D disp;
+            uniform float dispFactor;
+            uniform float intensity;
+            void main() {
+                vec2 uv = vUv;
+                vec4 disp = texture2D(disp, uv);
+                vec2 distortedPosition1 = vec2(uv.x + dispFactor * (disp.r * intensity), uv.y);
+                vec2 distortedPosition2 = vec2(uv.x - (1.0 - dispFactor) * (disp.r * intensity), uv.y);
+                vec4 _texture1 = texture2D(texture1, distortedPosition1);
+                vec4 _texture2 = texture2D(texture2, distortedPosition2);
+                gl_FragColor = mix(_texture1, _texture2, dispFactor);
+            }
+        `
+    }
+    // Можно добавить другие эффекты позже
+};
